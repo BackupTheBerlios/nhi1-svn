28a229b828f8f556896f7fb924f3c34383cc0ac6
aotto1968 <aotto1968@60b7bcc9-9579-0410-b65a-8c7028a52a44>
2009-12-03 09:13:41 +0000

    neu

---
+++ trunk/theLink/acmds/acut.h
@@ -0,0 +1,52 @@
+/**
+ *  \file       acmds/acut.h
+ *  \brief      \$Id: acut.h 507 2009-11-28 15:18:46Z dev1usr $
+ *  
+ *  (C) 2004 - NHI - #1 - Project - Group
+ *  
+ *  \version    \$Rev: 507 $
+ *  \author     EMail: aotto1968 at users.sourceforge.net
+ *  \attention  this software has GPL permissions to copy
+ *              please contact AUTHORS for additional information
+ */
+
+/** \addtogroup acut
+ * \{
+ */
+
+#ifndef CONTEXT_H
+#define CONTEXT_H
+
+#include "msgque.h"
+#include "config.h"
+
+/*****************************************************************************/
+/*                                                                           */
+/*                              context_basic                                */
+/*                                                                           */
+/*****************************************************************************/
+
+/// \brief the local \b context of the \ref acut tool
+struct CutCtxS {
+
+  struct MqS *	msgque;	      ///< \msgqueI
+  MQ_INT	fields[256];  ///< fields used to cut, from the <tt>-f \b FIELDS</tt> option
+
+};
+
+/*****************************************************************************/
+/*                                                                           */
+/*                                context_init                               */
+/*                                                                           */
+/*****************************************************************************/
+
+enum MqErrorE CutCreate (
+  struct MqConfigS  * config,
+  struct MqBufferLS ** argvP,
+  struct MqBufferLS ** alfaP,
+  struct MqS ** msgqueP
+);
+
+#endif /* CONTEXT_H */
+
+/** \} server */
