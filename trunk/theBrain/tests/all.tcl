dc9c7a43a09acd6133ba8ff1ff292402504bff17
aotto1968 <aotto1968@60b7bcc9-9579-0410-b65a-8c7028a52a44>
2009-12-03 09:13:54 +0000

    neu

--- trunk/theBrain/tests/all.tcl
+++ trunk/theBrain/tests/all.tcl
@@ -11,7 +11,7 @@
 #§
 
 set TS_SCRIPT [info script]
-source [file join [file dirname [info script]] library.tcl]
+source [file join [file dirname [info script]] brain.tcl]
 
 singleProcess false
 
@@ -20,26 +20,26 @@ singleProcess false
 testsDirectory [file normalize [file dirname [info script]]]
 if {[file tail [pwd]] ne "tests"} {
   ## check if I am in the parent of tests directory
-    if {[file isdirectory [file join . tests]]} {
-	cd [file join . tests]
-    } else {
-	cd [file dirname [info script]]
-    }
+  if {[file isdirectory [file join . tests]]} {
+    cd [file join . tests]
+  } else {
+    cd [file dirname [info script]]
+  }
 }
 
 ## Darwin need to set the variable DYLD_LIBRARY_PATH first
-if {$tcl_platform(os) eq "Darwin"} {
-    set ROOT    [file dirname [file dirname [file normalize [info script]]]]
-    set BUILD   [file dirname [pwd]]
-    set T1	[file nativename [file normalize [file join $ROOT src .libs]]]
-    set T2	[file nativename [file normalize [file join $BUILD src .libs]]]
-    if {[info exists env(DYLD_LIBRARY_PATH)]} {
-	set env(DYLD_LIBRARY_PATH) "$T1:$T2:$env(DYLD_LIBRARY_PATH)"
-    } else {
-	set env(DYLD_LIBRARY_PATH) "$T1:$T2"
-    }
-    unset ROOT BUILD T1 T2
-}
+#if {$tcl_platform(os) eq "Darwin"} {
+#    set ROOT    [file dirname [file dirname [file normalize [info script]]]]
+#    set BUILD   [file dirname [pwd]]
+#    set T1	[file nativename [file normalize [file join $ROOT src .libs]]]
+#    set T2	[file nativename [file normalize [file join $BUILD src .libs]]]
+#    if {[info exists env(DYLD_LIBRARY_PATH)]} {
+#	set env(DYLD_LIBRARY_PATH) "$T1:$T2:$env(DYLD_LIBRARY_PATH)"
+#    } else {
+#	set env(DYLD_LIBRARY_PATH) "$T1:$T2"
+#    }
+#    unset ROOT BUILD T1 T2
+#}
 
 tcltest::verbose {pass body error}
 
