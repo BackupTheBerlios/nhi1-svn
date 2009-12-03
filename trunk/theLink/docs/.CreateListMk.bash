f867167d38eaefd531f93f87e8e4430170a56ee9
aotto1968 <aotto1968@60b7bcc9-9579-0410-b65a-8c7028a52a44>
2009-12-03 11:48:43 +0000

    neu

--- trunk/theLink/docs/.CreateListMk.bash
+++ trunk/theLink/docs/.CreateListMk.bash
@@ -1,15 +1,4 @@
 #!/bin/bash
-#+
-#§  \file       win/.CreateMakefileAm.bash
-#§  \brief      \$Id:$
-#§  
-#§  (C) 2009 - LibMsgque - Project - Group - 2
-#§  
-#§  \version    \$Rev:$
-#§  \author     EMail: aotto1968 at users.sourceforge.net
-#§  \attention  this software has GPL permissions to copy
-#§              please contact AUTHORS for additional information
-#§
 
 echo 'DX_DIST = \' > list.mk
 
