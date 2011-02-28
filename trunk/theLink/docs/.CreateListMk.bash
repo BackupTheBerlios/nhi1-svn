ec5a346ceba1acd6199e16e8a9570d8ce7fafb9b
aotto1968 <aotto1968@60b7bcc9-9579-0410-b65a-8c7028a52a44>
2011-02-28 17:53:10 +0000

    neu

--- trunk/theLink/docs/.CreateListMk.bash
+++ trunk/theLink/docs/.CreateListMk.bash
@@ -2,7 +2,7 @@
 
 echo 'DX_DIST = \' > list.mk
 
-for f in $(svn list -R 2>&1) ; do
+for f in $(svn list -R -r HEAD 2>&1) ; do
   [[ "$f" == "doxygen.sh" ]] && continue
   test -d $f && continue
   echo "$f \\" >> list.mk
