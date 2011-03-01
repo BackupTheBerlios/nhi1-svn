f6ecd91fa9fd831d6c26d7327eda5786481f843a
aotto1968 <aotto1968@60b7bcc9-9579-0410-b65a-8c7028a52a44>
2011-03-01 08:46:18 +0000

    neu

--- trunk/theLink/libmsgque/msgque.h
+++ trunk/theLink/libmsgque/msgque.h
@@ -122,11 +122,11 @@
    to nothing for earlier compilers.  */
 
 #if defined(_MSC_VER) || (__GNUC__ == 2 && __GNUC_MINOR__ < 96)
-#define likely(x)       __builtin_expect((x),1)
-#define unlikely(x)     __builtin_expect((x),0)
-#else
 #define likely(x)       !!(x)
 #define unlikely(x)     !!(x)
+#else
+#define likely(x)       __builtin_expect((x),1)
+#define unlikely(x)     __builtin_expect((x),0)
 #endif
 
 #include <stdio.h>
