f06707f1e08b236fdcfd8600b3f280f7a41ec325
aotto1968 <aotto1968@60b7bcc9-9579-0410-b65a-8c7028a52a44>
2013-03-15 13:42:17 +0000

    neu

--- trunk/theLink/rubymsgque/sys_ruby.c
+++ trunk/theLink/rubymsgque/sys_ruby.c
@@ -187,7 +187,7 @@ void NS(MqS_Sys_Init)(void) {
   MqLal.SysExit	    = SysExit;
   //MqLal.SysWait = SysWait;
 
-  // deprecated: MqLal.SysSelect   = (MqSysSelectF) rb_thread_select;
-  MqLal.SysSelect   = (MqSysSelectF) rb_thread_fd_select;
+  MqLal.SysSelect   = (MqSysSelectF) rb_thread_select;
+  //MqLal.SysSelect   = (MqSysSelectF) rb_thread_fd_select;
 }
 
